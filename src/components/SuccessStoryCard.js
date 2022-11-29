38a6dd98bb2c91e2224f0c718039c61a0bca5b3c
IVKrylova <ivk.lis@yandex.ru>
2022-11-29 22:39:38 +0300

    styles: change styles

--- src/components/SuccessStoryCard.js
+++ src/components/SuccessStoryCard.js
@@ -29,7 +29,10 @@ export default class SuccessStoryCard {
   }
 
   _setHiddenClass() {
-    if (this._id === 0) this._elementTextContent.classList.remove('success-story-text__content_hidden');
+    if (this._id === 0) {
+      this._elementTextContent.classList.remove('success-story-text__content_hidden');
+      this._bottomLine.classList.remove('success-story-text__bottom-line_hidden');
+    }
   }
 
   _setEventListeners() {
@@ -42,12 +45,17 @@ export default class SuccessStoryCard {
       const textContentlist = document.querySelectorAll('.success-story-text__content');
 
       textContentlist.forEach((el) => {
-        const elId = el.parentNode.getAttribute('id');
+        // eslint-disable-next-line no-shadow
+        const container = el.parentNode;
+        const elId = container.getAttribute('id');
+        const bottomLine = container.querySelector('.success-story-text__bottom-line');
 
         if (elId === id) {
           el.classList.remove('success-story-text__content_hidden');
+          bottomLine.classList.remove('success-story-text__bottom-line_hidden');
         } else {
           el.classList.add('success-story-text__content_hidden');
+          bottomLine.classList.add('success-story-text__bottom-line_hidden');
         }
       });
     });
@@ -60,6 +68,7 @@ export default class SuccessStoryCard {
     this._elementSubtitle = this._element.querySelector('.success-story-subtitle');
     this._elementText = this._element.querySelector('.success-story-text__text');
     this._elementTextContent = this._element.querySelector('.success-story-text__content');
+    this._bottomLine = this._element.querySelector('.success-story-text__bottom-line');
 
     this._element.setAttribute('id', this._id);
     this._elementImg.src = this._img;
