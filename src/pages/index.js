6c17a3a43b267916cb25520671f37dd6fb9d78dc
IVKrylova <ivk.lis@yandex.ru>
2022-11-30 12:11:31 +0300

    feat: add html and css code to popup with form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import './index.scss';
-
 import PopupWithForm from '../components/PopupWithForm.js';
 import FormValidator from '../components/FormValidator';
-
 import {
   popupSelector,
   formSelector,
   configFormValidator,
 } from '../utils/constants';
+/* button for the oppening popup */
+const button = document.querySelector('.button');
 
 const submitHandlerForm = (data) => {
   console.log(data);
@@ -18,3 +18,8 @@ popupWithForm.setEventListeners();
 
 const FormInPopupValidator = new FormValidator(configFormValidator, '#form');
 FormInPopupValidator.enableValidation();
+
+/* to open popup */
+button.addEventListener('click', () => {
+  popupWithForm.open();
+});
